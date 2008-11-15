bd8d21238b30ee02aa4b79b0d5bd6814d9fe2d70
Tim Andersen <timander@gmail.com>
2008-11-14 22:18:27 -0600

    using a User instead of an Account

--- redtail/db/schema.rb
+++ redtail/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081028131624) do
+ActiveRecord::Schema.define(:version => 20081115033257) do
 
   create_table "accounts", :force => true do |t|
     t.string   "email"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(:version => 20081028131624) do
     t.float    "price"
     t.boolean  "open"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "password"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
