2509bb226c97a42b40581f154f918bb10024bdec
Luke Amdor <luke@absoluterubbish.net>
2008-12-06 10:26:44 -0600

    Fixed oddity of merge

--- redtail/test/functional/user_controller_test.rb
+++ redtail/test/functional/user_controller_test.rb
@@ -1,8 +1,54 @@
 require 'test_helper'
 
-class MainControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+class UserControllerTest < ActionController::TestCase
+  fixtures :users
+
+  def test_logout
+    @request.session[:uid] = "something"
+
+    get :logout
+
+    assert_response :success
+    assert_nil @request.session[:uid]
+    assert_equal "you are logged out", @response.body
+  end
+
+  def test_get_login
+    get :login
+
+    assert_response :success
+  end
+
+  def test_post_login_good
+    post :login, { "login" => "ron", "password" => "my_password" }
+
+    assert_equal users(:ron).id, @request.session[:uid]
+    assert_response :redirect
+    assert_redirected_to new_reservation_url
+  end
+  
+  def test_post_login_good_redirected_back_to_url
+    @request.session[:requested_url] = { :controller => 'restuarants'}
+    post :login, { "login" => "ron", "password" => "my_password" }
+
+    assert_equal users(:ron).id, @request.session[:uid]
+    assert_response :redirect
+    assert_redirected_to :controller => 'restuarants'
+  end
+  
+  def test_post_login_bad_password
+    post :login, { "login" => "ron", "password" => "BAD_PASSWORD" }
+
+    assert_nil @request.session[:uid]
+    assert_response :success
+    assert_equal "Invalid login/password", assigns["auth_error"]
+  end
+
+  def test_post_login_bad_user
+    post :login, { "login" => "bad_user", "password" => "my_password" }
+
+    assert_nil @request.session[:uid]
+    assert_response :success
+    assert_equal "Invalid login/password", assigns["auth_error"]
   end
 end
--- redtail/test/unit/reservation_test.rb
+++ redtail/test/unit/reservation_test.rb
@@ -1,55 +1,8 @@
 require 'test_helper'
 
 class ReservationTest < ActiveSupport::TestCase
-  fixtures :users
 
-  def test_logout
-    @request.session[:uid] = "something"
-
-    get :logout
-
-    assert_response :success
-    assert_nil @request.session[:uid]
-    assert_equal "you are logged out", @response.body
+  def test_dummy
+    assert true
   end
-
-  def test_get_login
-    get :login
-
-    assert_response :success
-  end
-
-  def test_post_login_good
-    post :login, { "login" => "ron", "password" => "my_password" }
-
-    assert_equal users(:ron).id, @request.session[:uid]
-    assert_response :redirect
-    assert_redirected_to :controller => 'reservation'
-  end
-  
-  def test_post_login_good_redirected_back_to_url
-    @request.session[:requested_url] = { :controller => 'restuarants'}
-    post :login, { "login" => "ron", "password" => "my_password" }
-
-    assert_equal users(:ron).id, @request.session[:uid]
-    assert_response :redirect
-    assert_redirected_to :controller => 'restuarants'
-  end
-  
-  def test_post_login_bad_password
-    post :login, { "login" => "ron", "password" => "BAD_PASSWORD" }
-
-    assert_nil @request.session[:uid]
-    assert_response :success
-    assert_equal "Invalid login/password", assigns["auth_error"]
-  end
-
-  def test_post_login_bad_user
-    post :login, { "login" => "bad_user", "password" => "my_password" }
-
-    assert_nil @request.session[:uid]
-    assert_response :success
-    assert_equal "Invalid login/password", assigns["auth_error"]
-  end
-  
 end
