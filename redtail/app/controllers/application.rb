10c34d29c256bc78c5cf0587db595042ef539cc4
Tim Andersen <timander@gmail.com>
2009-01-07 13:11:08 -0600

    added items to the backlog; added elements to the reservation form

--- redtail/app/controllers/application.rb
+++ redtail/app/controllers/application.rb
@@ -7,29 +7,29 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '0ef170a78b236246ea72459155a1efd8'
-  
+
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
-  
+
 
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_hungryhawk_session_id'
 
   before_filter :user_is_authenticated?
-  
+
   private
-  
+
   def user_is_authenticated?
     unless (session[:uid])
       flash[:requested_url] = params
       redirect_to login_url
     end
   end
-  
+
   def session_user
     @user ||= User.find_by_id(session[:uid])
   end
-  
+
 end
