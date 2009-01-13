64ab6526eb9ea61ac4c7e39c985cf7d3c29d61e4
Tim Andersen <timander@gmail.com>
2009-01-12 23:06:49 -0600

    playing around with form cosmetics changes

--- redtail/app/models/reservation.rb
+++ redtail/app/models/reservation.rb
@@ -3,7 +3,6 @@ class Reservation < ActiveRecord::Base
   belongs_to :restaurant
   
   validates_presence_of :lunch_period, :on => :create, :message => "can't be blank"
-  validates_presence_of :take_out, :on => :create, :message => "can't be blank"
   validates_presence_of :restaurant, :on => :create, :message => "can't be blank"
   validates_presence_of :user, :on => :create, :message => "can't be blank"
 
