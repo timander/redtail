e3bebff536313e91d1319797ae139105372154ab
Tim Andersen <timander@gmail.com>
2009-01-14 13:27:52 -0600

    added a finder for the next restaurant (with lamdor's help)

--- redtail/app/models/restaurant.rb
+++ redtail/app/models/restaurant.rb
@@ -4,4 +4,9 @@ class Restaurant < ActiveRecord::Base
   def name_on_date
     "#{name} on #{date}"
   end
+  
+  def self.find_next
+    self.find(:first, :conditions => ["date > ?", Date.today], :order => "date asc") 
+  end
+  
 end
--- redtail/test/unit/restaurant_test.rb
+++ redtail/test/unit/restaurant_test.rb
@@ -1,8 +1,12 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class RestaurantTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  def test_find_next
+    restaurant_in_past = Restaurant.create(:name => 'past', :date => 1.day.ago)
+    next_restaurant = Restaurant.create(:name => 'next', :date => Date.tomorrow)
+    restaurant_in_future = Restaurant.create(:name => 'future', :date => 2.days.from_now)
+    assert_equal next_restaurant, Restaurant.find_next
   end
+
 end
