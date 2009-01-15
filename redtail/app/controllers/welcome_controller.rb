e7224b92aa4c176671dce814ce246d269b6530f3
Tim Andersen <timander@gmail.com>
2009-01-15 00:36:13 -0600

    cosmetic changes to welcome page

--- redtail/app/controllers/welcome_controller.rb
+++ redtail/app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   skip_filter :user_is_authenticated?
 
   def index
-    @restaurant = Restaurant.find_next
+    @next_restaurant = Restaurant.find_next
     @first_lunch = LunchPeriod.find_by_period(1)
     @second_lunch = LunchPeriod.find_by_period(2)
   end
