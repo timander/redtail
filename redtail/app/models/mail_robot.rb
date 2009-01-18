1a8ac6ea706db504bc66692962cff717de617bc4
Tim Andersen <timander@gmail.com>
2009-01-18 16:42:11 -0600

    removed html content type from email, removed unnecessary comments

--- redtail/app/models/mail_robot.rb
+++ redtail/app/models/mail_robot.rb
@@ -7,10 +7,7 @@ class MailRobot < ActionMailer::Base
       from "The Hungry Hawk <admin@hungryhawk.org>"
       recipients user.email
       subject "Confirm email address"
-      content_type 'text/html'
-
-      #body :key => options[:key], :domain => options[:domain]
-      # email body substitutions go here
+      
       @body["email"] = user.email
       @body["hash"] = hash
       @body["url"] = url
