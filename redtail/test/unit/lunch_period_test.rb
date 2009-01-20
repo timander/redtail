0c59d47698a70cb238758b09e018dd2e601034d5
Luke Amdor <luke@absoluterubbish.net>
2009-01-19 20:48:02 -0600

    Added test for LunchPeriod#to_s

--- redtail/test/unit/lunch_period_test.rb
+++ redtail/test/unit/lunch_period_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class LunchPeriodTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  test "to_s" do
+    lunch_period = LunchPeriod.new(:name => "First Period",
+                                   :begin_end_times => "12:00 pm - 12:20 pm")
+    assert_equal "First Period [12:00 pm - 12:20 pm]", lunch_period.to_s
   end
+  
 end
