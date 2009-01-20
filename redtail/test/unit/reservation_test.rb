56b82f35abf12cc429929e0f3d2140c4987d49d5
Luke Amdor <luke@absoluterubbish.net>
2009-01-19 20:55:10 -0600

    Added tests for validates_presence_of for Reservation

--- redtail/test/unit/reservation_test.rb
+++ redtail/test/unit/reservation_test.rb
@@ -2,7 +2,33 @@ require 'test_helper'
 
 class ReservationTest < ActiveSupport::TestCase
 
-  def test_dummy
-    assert true
+  def setup
+    @options = {
+      :user => User.new,
+      :restaurant => Restaurant.new,
+      :lunch_period => LunchPeriod.new
+    }
   end
+
+  test "requires user"  do
+    @options.delete(:user)
+    reservation = Reservation.create(@options)
+    assert !reservation.valid?
+    assert reservation.errors.on(:user)
+  end
+  
+  test "requires restaurant"  do
+    @options.delete(:restaurant)
+    reservation = Reservation.create(@options)
+    assert !reservation.valid?
+    assert reservation.errors.on(:restaurant)
+  end
+  
+  test "requires lunch period"  do
+    @options.delete(:lunch_period)
+    reservation = Reservation.create(@options)
+    assert !reservation.valid?
+    assert reservation.errors.on(:lunch_period)
+  end
+  
 end
