c1e3774bab91ba9f5c4ce22ef28da95a3276706c
Luke Amdor <luke@absoluterubbish.net>
2009-01-19 22:53:43 -0600

    Added test for MailRobot

--- redtail/app/models/mail_robot.rb
+++ redtail/app/models/mail_robot.rb
@@ -1,16 +1,16 @@
 class MailRobot < ActionMailer::Base
 
   helper :application
- 
+  
   def confirmation_email(user, hash, url)
+
+    from "The Hungry Hawk <admin@hungryhawk.org>"
+    recipients user.email
+    subject "Confirm email address"
     
-      from "The Hungry Hawk <admin@hungryhawk.org>"
-      recipients user.email
-      subject "Confirm email address"
-      
-      @body["email"] = user.email
-      @body["hash"] = hash
-      @body["url"] = url
+    @body["email"] = user.email
+    @body["hash"] = hash
+    @body["url"] = url
 
   end
 
--- redtail/test/unit/mail_robot_test.rb
+++ redtail/test/unit/mail_robot_test.rb
@@ -2,8 +2,26 @@ require 'test_helper'
 
 class MailRobotTest < ActionMailer::TestCase
   tests MailRobot
-  # replace this with your real tests
-  def test_truth
-    assert true
+
+  test "create confirmation email" do
+    user = User.new(:email => "test@test.com")
+    hash = "some-hash"
+    url = "some-url/"
+    
+    @expected.from = "The Hungry Hawk <admin@hungryhawk.org>"
+    @expected.to = "test@test.com"
+    @expected.subject = "Confirm email address"
+    @expected.body = <<EOB
+test@test.com,
+ 
+Thank you for registering with our website. To confirm your email address, please visit the following address:
+
+some-url/confirm_email/some-hash
+
+- The Hungry Hawk
+
+EOB
+
+    assert_equal @expected.encoded, MailRobot.create_confirmation_email(user, hash, url).encoded
   end
 end
