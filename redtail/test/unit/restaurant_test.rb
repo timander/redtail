14e211e7e63042547875a7fff716c8bec0a8e051
Luke Amdor <luke@absoluterubbish.net>
2009-01-19 20:45:53 -0600

    Added test for name_on_date

--- redtail/test/unit/restaurant_test.rb
+++ redtail/test/unit/restaurant_test.rb
@@ -2,6 +2,12 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class RestaurantTest < ActiveSupport::TestCase
 
+  def test_name_on_date
+    restaurant = Restaurant.new(:name => "Some Restaurant",
+                                :date => Date.today)
+    assert_equal "Some Restaurant on #{Date.today}", restaurant.name_on_date
+  end
+
   def test_find_next
     restaurant_in_past = Restaurant.create(:name => 'past', :date => Date.yesterday)
     next_restaurant = Restaurant.create(:name => 'next', :date => Date.tomorrow)
