051743ba250926164d3936610a1d90ff161e8f75
Luke Amdor <luke@absoluterubbish.net>
2009-01-19 20:43:06 -0600

    Updated test to work after 7 o'clock on any night

--- redtail/test/unit/restaurant_test.rb
+++ redtail/test/unit/restaurant_test.rb
@@ -3,9 +3,10 @@ require File.dirname(__FILE__) + '/../test_helper'
 class RestaurantTest < ActiveSupport::TestCase
 
   def test_find_next
-    restaurant_in_past = Restaurant.create(:name => 'past', :date => 1.day.ago)
+    restaurant_in_past = Restaurant.create(:name => 'past', :date => Date.yesterday)
     next_restaurant = Restaurant.create(:name => 'next', :date => Date.tomorrow)
     restaurant_in_future = Restaurant.create(:name => 'future', :date => 2.days.from_now)
+
     assert_equal next_restaurant, Restaurant.find_next
   end
 
