23657feecb30d782b82097fb402c530b9e1fb79b
Tim Andersen <timander@gmail.com>
2009-01-22 21:39:08 -0600

    showed email address on welcome page when logged in

--- redtail/app/controllers/welcome_controller.rb
+++ redtail/app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   skip_filter :user_is_authenticated?
 
   def index
+    @user = session_user
     @next_restaurant = Restaurant.find_next
     @first_lunch = LunchPeriod.find_by_period(1)
     @second_lunch = LunchPeriod.find_by_period(2)
--- redtail/test/functional/welcome_controller_test.rb
+++ redtail/test/functional/welcome_controller_test.rb
@@ -1,12 +1,29 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  fixtures :restaurants
+  fixtures :restaurants, :users, :lunch_periods
 
-  def test_index
+  test 'index as normal user logged in' do
+    logged_in_as users(:ron)
     get :index
         
     assert_response :success
+    
+    assert_equal(users(:ron), assigns["user"])
+    assert_equal(lunch_periods(:first), assigns["first_lunch"])
+    assert_equal(lunch_periods(:second), assigns["second_lunch"])
+    assert_equal(restaurants(:greek), assigns["next_restaurant"])
+  end
+
+
+  test 'index not logged in' do
+    get :index
+        
+    assert_response :success
+    
+    assert_equal(nil, assigns["user"])
+    assert_equal(lunch_periods(:first), assigns["first_lunch"])
+    assert_equal(lunch_periods(:second), assigns["second_lunch"])
     assert_equal(restaurants(:greek), assigns["next_restaurant"])
   end
   
